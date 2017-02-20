1b6097796787f41b6b7c7168937aed7418a2cc79
林志涛 <linzhitao@linzhitaodeMacBook-Pro.local>
2017-02-20 22:21:48 +0800

    %date%%time%

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #coding=utf8
 
-TOKEN = ''
-APP_ID = ''
-SECRET_KEY = ''
-TULING_KEY = ''
+TOKEN = 'Token'
+APP_ID = 'wx68d57a9432660cb2'
+SECRET_KEY = '03481467f2ce08b345a7634d0794a043'
+TULING_KEY = '8edce3ce905a4c1dbb965e6b35c3834d'
 ADMIN_OAUTH = 'admin_oauth'
 WELCOME_WORD = u'''\
 欢迎关注我的微信号！
@@ -11,7 +11,7 @@ WELCOME_WORD = u'''\
 目录： 获取文章目录的网址
 帮助： 获取本信息
 其他： 图灵机器人会陪你聊天'''
-INDEX_URL = 'www.baidu.com'
+INDEX_URL = 'mumushow.applinzi.com'
 REPLY_DICT = {
     u'目录': '点这里-> ' + INDEX_URL,
     u'帮助': WELCOME_WORD,
